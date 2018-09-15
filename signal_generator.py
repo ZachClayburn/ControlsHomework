e6a649e12d15f52e3b3d001852c8d67aa485c824
Zach Clayburn <zachclayburn@gmail.com>
2018-09-15 13:34:52 -0600

    Added constant signal generator

--- signal_generator.py
+++ signal_generator.py
@@ -38,6 +38,14 @@ def sin(frequency: float=1, amplitude: float=1, y_offset: float=0,
         time += t_step
 
 
+def constant(frequency: float=1, amplitude: float=1, y_offset: float=0,
+             t_start: float=0, t_step: float=0.1, t_final: float=math.inf) -> Iterable[float]:
+    time = t_start
+    while time <= t_final:
+        yield y_offset
+        time += t_step
+
+
 if __name__ == '__main__':
     for t in random(t_final=5, frequency=5):
         print(t)
