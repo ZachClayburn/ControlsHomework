670522fd2f93d0a854cc406b02d5e11f413f2213
Zach Clayburn <zachclayburn@gmail.com>
2018-09-28 12:35:32 -0600

    Refactored the signal generator

--- signal_generator.py
+++ signal_generator.py
@@ -1,51 +1,47 @@
 import math
 import numpy as np
-from typing import Iterable
+from typing import Iterable, Callable
 
 
-def square(frequency: float=1, amplitude: float=1, y_offset: float=0,
-           t_start: float=0, t_step: float=0.1, t_final: float=math.inf) -> Iterable[float]:
-    time = t_start
-    while time < t_final:
-        if time % (1.0/frequency) <= 0.5/frequency:
-            yield y_offset + amplitude
-        else:
-            yield y_offset - amplitude
-        time += t_step
-
-
-def sawtooth(frequency: float=1, amplitude: float=1, y_offset: float=0,
-             t_start: float=0, t_step: float=0.1, t_final: float=math.inf) -> Iterable[float]:
+def generator(signal_fun: Callable[[float, float, float, float], float],
+              frequency: float = 1, amplitude: float = 1, y_offset: float = 0,
+              t_start: float = 0, t_step: float = 0.1, t_final: float = math.inf) -> Iterable[float]:
     time = t_start
     while time <= t_final:
-        yield 4 * amplitude * frequency * (time % 1.0 / frequency) - amplitude + y_offset
+        yield signal_fun(time, frequency, amplitude, y_offset)
         time += t_step
 
 
-def random(frequency: float=1, amplitude: float=1, y_offset: float=0,
-           t_start: float=0, t_step: float=0.1, t_final: float=math.inf) -> Iterable[float]:
-    time = t_start
-    while time <= t_final:
-        yield np.sqrt(amplitude) * np.random.rand() + y_offset
-        time += t_step
+def continuous(signal_fun: Callable[[float, float, float, float], float],
+               frequency: float = 1, amplitude: float = 1, y_offset: float = 0) -> Callable[[float], float]:
+    def out_fun(time: float) -> float:
+        return signal_fun(time, frequency, amplitude, y_offset)
+
+    return out_fun
 
 
-def sin(frequency: float=1, amplitude: float=1, y_offset: float=0,
-        t_start: float=0, t_step: float=0.1, t_final: float=math.inf) -> Iterable[float]:
-    time = t_start
-    while time <= t_final:
-        yield amplitude * np.sin(2 * np.pi * frequency * time) + y_offset
-        time += t_step
+def square(time: float, frequency: float = 1, amplitude: float = 1, y_offset: float = 0) -> float:
+    if time % (1.0 / frequency) <= 0.5 / frequency:
+        return y_offset + amplitude
+    else:
+        return y_offset - amplitude
 
 
-def constant(frequency: float=1, amplitude: float=1, y_offset: float=0,
-             t_start: float=0, t_step: float=0.1, t_final: float=math.inf) -> Iterable[float]:
-    time = t_start
-    while time <= t_final:
-        yield y_offset
-        time += t_step
+def sawtooth(time: float, frequency: float = 1, amplitude: float = 1, y_offset: float = 0) -> float:
+    return 4 * amplitude * frequency * (time % 1.0 / frequency) - amplitude + y_offset
+
+
+def random(time: float, frequency: float = 1, amplitude: float = 1, y_offset: float = 0) -> float:
+    return np.sqrt(amplitude) * np.random.rand() + y_offset
+
+
+def sin(time: float, frequency: float = 1, amplitude: float = 1, y_offset: float = 0) -> float:
+    return amplitude * np.sin(2 * np.pi * frequency * time) + y_offset
+
+
+def constant(time: float, frequency: float = 1, amplitude: float = 1, y_offset: float = 0) -> float:
+    return amplitude
 
 
 if __name__ == '__main__':
-    for t in random(t_final=5, frequency=5):
-        print(t)
+    pass
