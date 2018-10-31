039a34dfb2c28e3a0af3205ac5cd6716a47e439c
Zach Clayburn <zachclayburn@gmail.com>
2018-10-31 09:19:28 -0600

    Fixed some bug from HW5 in preperation for HW7

--- HW5.py
+++ HW5.py
@@ -73,7 +73,7 @@ def e_8_e():
                        proportional_gain_theta, derivative_gain_theta)
     # a square wave with magnitude 0.25±0.15 meters and frequency 0.01 Hz
     requests = sg.generator(sg.square, amplitude=0.15, y_offset=0.25, frequency=0.01,
-                            t_step=bnb.seconds_per_sim_step, t_final=5)
+                            t_step=bnb.seconds_per_sim_step, t_final=30)
     handle = bnb.view_animation(requests)
     Sim.Animations.plt.waitforbuttonpress()
     Sim.Animations.plt.close()
@@ -104,7 +104,7 @@ def e_8_f():
                        proportional_gain_z, derivative_gain_z,
                        proportional_gain_theta, derivative_gain_theta, max_force)
     # a square wave with magnitude 0.25±0.15 meters and frequency 0.01 Hz
-    requests = sg.generator(sg.constant, amplitude=0.25, t_step=bnb.seconds_per_sim_step, t_final=5)
+    requests = sg.generator(sg.constant, amplitude=0.0, t_step=bnb.seconds_per_sim_step, t_final=10)
     handle = bnb.view_animation(requests)
     Sim.Animations.plt.waitforbuttonpress()
     Sim.Animations.plt.close()
@@ -146,13 +146,13 @@ def f_8_a():
 
 def f_8_f():
     pvt = Sim.PlanarVTOL()
-    rise_time_h = 8
+    rise_time_h = 1
     natural_frequency_h = np.pi / (2 * rise_time_h * (1 - damping_ratio**2)**(1/2))
     c_h = 1 / (pvt.dynamics.center_mass + 2 * pvt.dynamics.wing_mass)
     proportional_gain_h = natural_frequency_h**2 / c_h
     derivative_gain_h = 2 * damping_ratio * natural_frequency_h / c_h
 
-    rise_time_theta = 0.8
+    rise_time_theta = 0.08
     natural_frequency_theta = np.pi / (2 * rise_time_theta * (1 - damping_ratio**2)**(1/2))
     j_center = pvt.dynamics.center_moi
     wing_mass = pvt.dynamics.wing_mass
@@ -177,7 +177,7 @@ def f_8_f():
     request = zip(
         sg.generator(sg.constant, y_offset=0, t_step=pvt.seconds_per_sim_step, t_final=10),
         # a square wave with magnitude 3 ± 2.5 meters and frequency 0.08 Hz
-        sg.generator(sg.sin, frequency=0.08, y_offset=3, amplitude=2.5, t_step=pvt.seconds_per_sim_step, t_final=10),
+        sg.generator(sg.sin, frequency=0.1, y_offset=0, amplitude=1, t_step=pvt.seconds_per_sim_step, t_final=10),
     )
     handle = pvt.view_animation(request)
     Sim.Animations.plt.waitforbuttonpress()
@@ -188,6 +188,7 @@ def f_8_f():
 def f_8_g():
     pvt = Sim.PlanarVTOL()
     rise_time_h = 8
+    max_force = 10
     natural_frequency_h = np.pi / (2 * rise_time_h * (1 - damping_ratio**2)**(1/2))
     c_h = 1 / (pvt.dynamics.center_mass + 2 * pvt.dynamics.wing_mass)
     proportional_gain_h = natural_frequency_h**2 / c_h
@@ -214,7 +215,8 @@ def f_8_g():
     pvt.add_controller(PD.PlanarVTOL,
                        proportional_gain_h, derivative_gain_h,
                        proportional_gain_z, derivative_gain_z,
-                       proportional_gain_theta, derivative_gain_theta)
+                       proportional_gain_theta, derivative_gain_theta,
+                       max_force)
     request = zip(
         sg.generator(sg.constant, y_offset=0, t_step=pvt.seconds_per_sim_step, t_final=10),
         # a square wave with magnitude 3 ± 2.5 meters and frequency 0.08 Hz
@@ -230,9 +232,9 @@ if __name__ == '__main__':
     # d_7_d()
     # d_8_a()
     # d_8_b()
-    e_8_e()
-    e_8_f()
+    # e_8_e()
+    # e_8_f()
     # f_7_d()
     # f_8_a()
-    # f_8_f()
+    f_8_f()
     # f_8_g()
--- Simulation/parameters.py
+++ Simulation/parameters.py
@@ -1,14 +1,22 @@
+import random
+
+
+randomness_parameter = 0.0
+upper = 1 + randomness_parameter
+lower = 1 - randomness_parameter
+
+
 class GlobalParameters:
-    sample_rate = 0.04
+    sample_rate = 0.01
     gravity = 9.8
 
 
 class MassSpringDamper:
     width = 1.0
     height = 1.0
-    mass = 5.0
-    spring_const = 3.0
-    damping = 0.5
+    mass = 5.0 * random.uniform(lower, upper)
+    spring_const = 3.0 * random.uniform(lower, upper)
+    damping = 0.5 * random.uniform(lower, upper)
     state_0 = [
         [0.0],
         [0.0]
@@ -17,11 +25,11 @@ class MassSpringDamper:
 
 
 class BallAndBeam:
-    beam_length = 0.5
+    beam_length = 0.5 * random.uniform(lower, upper)
     beam_width = 0.01
-    beam_mass = 2.0
+    beam_mass = 2.0 * random.uniform(lower, upper)
     ball_radius = 0.06
-    ball_mass = 0.35
+    ball_mass = 0.35 * random.uniform(lower, upper)
     state_0 = [
         [beam_length / 2],
         [0.0],
@@ -36,11 +44,11 @@ class PlanarVTOL:
     body_height = 0.125
     wing_major_axis = 0.25
     wing_minor_axis = 0.1
-    wing_spacing = 0.3
-    center_mass = 1.0
-    center_moi = 0.0042
+    wing_spacing = 0.3 * random.uniform(lower, upper)
+    center_mass = 1.0 * random.uniform(lower, upper)
+    center_moi = 0.0042 * random.uniform(lower, upper)
     wing_mass = 0.25
-    drag = 0.1
+    drag = 0.1 * random.uniform(lower, upper)
     state_0 = [
         [0.0],
         [1.0],
