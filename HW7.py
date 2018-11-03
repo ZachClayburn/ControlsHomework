143c9660af2fbd74115b408eb73c5e7ea6afcc02
Zach Clayburn <zachclayburn@gmail.com>
2018-11-02 19:00:01 -0600

    Added PID to the ball and beam

--- HW7.py
+++ HW7.py
@@ -6,8 +6,7 @@ import Simulation.signal_generator as sg
 import Simulation.parameters as params
 
 
-def d_10_c():
-    params.randomness_parameter = 0
+def d_10():
     msd = Sim.MassSpringDamper()
     max_force = 2.0
     damping_ratio = 1 / (2 ** (1 / 2))
@@ -28,5 +27,53 @@ def d_10_c():
     return handle
 
 
+def e_10():
+    bnb = Sim.BallAndBeam()
+    max_force = 15.0
+    damping_ratio = 1 / (2 ** (1 / 2))
+
+    length = bnb.dynamics.beam_length
+    ball_mass = bnb.dynamics.ball_mass
+    beam_mass = bnb.dynamics.beam_mass
+    z_equilibrium = length / 2
+    gravity = bnb.dynamics.gravity
+    b_theta = length / ((beam_mass * length ** 2) / 3 + ball_mass * z_equilibrium ** 2)
+
+    #  Tuning variables
+    rise_time_theta = 1
+    bandwidth_separation = 10
+    integration_gain_theta = 0
+    integration_gain_z = 0
+    threshold_theta = np.inf
+    threshold_z = np.inf
+
+    natural_frequency_theta = np.pi / (2 * rise_time_theta * (1 - damping_ratio ** 2) ** (1 / 2))
+    proportional_gain_theta = natural_frequency_theta ** 2 / b_theta
+    derivative_gain_theta = 2 * damping_ratio * natural_frequency_theta / b_theta
+
+    rise_time_z = rise_time_theta * bandwidth_separation
+    natural_frequency_z = np.pi / (2 * rise_time_z * (1 - damping_ratio ** 2) ** (1 / 2))
+    proportional_gain_z = - natural_frequency_z ** 2 / gravity
+    derivative_gain_z = - 2 * damping_ratio * natural_frequency_z / gravity
+
+    bnb.add_controller(PID.BallAndBeam,
+                       proportional_gain_z, derivative_gain_z, integration_gain_z,
+                       proportional_gain_theta, derivative_gain_theta, integration_gain_theta,
+                       threshold_z, threshold_theta, max_force)
+
+    # a square wave with magnitude 0.25±0.15 meters and frequency 0.01 Hz
+    requests = sg.generator(sg.square, amplitude=0.15, y_offset=0.25, frequency=0.1,
+                            t_step=bnb.seconds_per_sim_step, t_final=30)
+    handle = bnb.view_animation(requests)
+    Sim.Animations.plt.waitforbuttonpress()
+    Sim.Animations.plt.close()
+    return handle
+
+
+def f_10():
+    pass
+
+
 if __name__ == '__main__':
-    d_10_c()
+    # d_10()
+    e_10()
--- PIDController.py
+++ PIDController.py
@@ -9,7 +9,7 @@ class MassSpringDamper(Simulation.Controller.MassSpringDamper):
 
     def __init__(self, system: Simulation.MassSpringDamper,
                  proportional_gain: float, derivative_gain: float, integrator_gain: float,
-                 max_force: float=np.inf):
+                 max_force: float = np.inf):
         super().__init__()
         self.dynamics = system.dynamics
         self.proportional_gain = proportional_gain
@@ -34,7 +34,6 @@ class MassSpringDamper(Simulation.Controller.MassSpringDamper):
         force = self.proportional_gain * z_error + self.integrator_gain * self.int - self.derivative_gain * z_dot
 
         self.prev_error = z_error
-        # print(f"Error:{z_error}, Integrator:{self.int}")
 
         # Saturation
         if force > self.max_force:
@@ -53,7 +52,7 @@ class BallAndBeam(Simulation.Controller.BallAndBeam):
     def __init__(self, system: Simulation.BallAndBeam,
                  proportional_gain_z: float, derivative_gain_z: float, integrator_gain_z: float,
                  proportional_gain_theta: float, derivative_gain_theta: float, integrator_gain_theta: float,
-                 max_force: float=np.inf):
+                 thresh_z: float = np.inf, thresh_theta: float = np.inf, max_force: float = np.inf):
         super().__init__()
         self.dynamics = system.dynamics
         self.proportional_gain_z = proportional_gain_z
@@ -63,9 +62,78 @@ class BallAndBeam(Simulation.Controller.BallAndBeam):
         self.derivative_gain_theta = derivative_gain_theta
         self.integrator_gain_theta = integrator_gain_theta
         self.max_force = max_force
+        self.thresh_theta = thresh_theta
+        self.thresh_z = thresh_z
 
-    def callback(self, request: Union[float, tuple]) -> np.ndarray:
-        pass
+        self.prev_z: float = 0.0
+        self.prev_error_z: float = 0.0
+        self.int_z: float = 0.0
+
+        self.prev_theta: float = 0.0
+        self.prev_error_theta: float = 0.0
+        self.int_theta: float = 0.0
+
+    def callback(self, request: float) -> np.ndarray:
+        theta_request = self._z_callback(request)
+        force = self._theta_callback(theta_request)
+
+        equilibrium_force = \
+            self.dynamics.gravity * \
+            (self.dynamics.ball_mass * self.dynamics.z /
+             self.dynamics.beam_length + self.dynamics.beam_mass / 2)
+
+        force += equilibrium_force
+
+        if force > self.max_force:
+            print(f'Saturation: {force}')
+            force = self.max_force
+        if force < -self.max_force:
+            print(f'Saturation: {force}')
+            force = -self.max_force
+
+        # print(f"Z Requested:{request} Force:{force}")
+
+        u = np.array([force])
+        return u
+
+    def _z_callback(self, z_request: float) -> float:
+        dt = self.dynamics.sample_rate
+
+        z = self.dynamics.z
+        error = z_request - z
+        z_dot = (z - self.prev_z) / dt
+        print(f"measured:{self.dynamics.zdot} calculated:{z_dot}")
+        error_dot = (error - self.prev_error_z) / dt
+
+        if abs(error_dot) < self.thresh_z:
+            self.int_z += (dt / 2) * (error + self.prev_error_z)
+
+        self.prev_z = z
+        self.prev_error_z = error
+
+        return \
+            self.proportional_gain_z * error + \
+            self.integrator_gain_z * self.int_z - \
+            self.derivative_gain_z * z_dot
+
+    def _theta_callback(self, theta_request: float) -> float:
+        dt = self.dynamics.sample_rate
+
+        theta = self.dynamics.theta
+        error = theta_request - theta
+        theta_dot = (theta - self.prev_theta) / dt
+        error_dot = (error - self.prev_error_theta) / dt
+
+        if abs(error_dot) < self.thresh_theta:
+            self.int_theta += (dt / 2) * (error + self.prev_error_theta)
+
+        self.prev_theta = theta
+        self.prev_error_theta = error
+
+        return \
+            self.proportional_gain_theta * error + \
+            self.integrator_gain_theta * self.int_z - \
+            self.proportional_gain_theta * theta_dot
 
 
 class PlanarVTOL(Simulation.Controller.PlanarVTOL):
@@ -74,7 +142,7 @@ class PlanarVTOL(Simulation.Controller.PlanarVTOL):
                  proportional_gain_h: float, derivative_gain_h: float, integrator_gain_h: float,
                  proportional_gain_z: float, derivative_gain_z: float, integrator_gain_z: float,
                  proportional_gain_theta: float, derivative_gain_theta: float, integrator_gain_theta: float,
-                 max_force: float=np.inf):
+                 max_force: float = np.inf):
         super().__init__()
         self.dynamics = system.dynamics
         self.proportional_gain_h = proportional_gain_h
